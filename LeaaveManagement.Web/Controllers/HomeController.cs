d4cc37c21e046610ae49e430bbfc2a7948294a83
Mario Velazquez <mario.velazquez@glennonbrothers.ie>
2023-04-06 14:25:22 +0100

    Add project files.

---
+++ LeaaveManagement.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using LeaaveManagement.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LeaaveManagement.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
